4bfb5f5868cc29706b2c544ac3731bfc679ba441
RajkeshaSolith <sajonelle@gmail.com>
2019-12-01 13:03:44 -0500

    Redid sql using phpmyadmin

--- schema.sql
+++ schema.sql
@@ -1,32 +1,74 @@
-DROP DATABASE IF EXISTS schema;
-CREATE DATABASE schema;
-USE schema;
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 01, 2019 at 01:50 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
-DROP TABLE IF EXISTS "Users";
-CREATE TABLE "Users" (
-  "id" int(11) NOT NULL auto_increment,
-  "firstname" char(35) NOT NULL default '',
-  "lastname" char(35) NOT NULL default '',
-  "password" char(35) NOT NULL default '',
-  "email" char(35) NOT NULL default '',
-  "date_joined" DATE NOT NULL default '',
-  PRIMARY KEY  ("id")
-) ENGINE=MyISAM AUTO_INCREMENT=4080 DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO "Users" VALUES (000,'Admin','Gai','password123','admin@bugme.com',2019-11-24);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-DROP TABLE IF EXISTS "Issues";
-CREATE TABLE "Issues" (
-  "id" int(11) NOT NULL auto_increment,
-  "title" char(35) NOT NULL default '',
-  "description" LONGTEXT NOT NULL default '',
-  "type" char(35) NOT NULL default '',
-  "priority" char(35) NOT NULL default '',
-  "status" char(35) NOT NULL default '',
-  "assigned_to" int(11) NOT NULL default '',
-  "created_by" int(11) NOT NULL default '',
-  "created" DATE NOT NULL default '',
-  "updated" DATE NOT NULL default '',
-  PRIMARY KEY  ("id")
-) ENGINE=MyISAM AUTO_INCREMENT=4080 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `schema`
+--
+CREATE DATABASE IF NOT EXISTS `schema` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `schema`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issues`
+--
+
+DROP TABLE IF EXISTS `issues`;
+CREATE TABLE IF NOT EXISTS `issues` (
+  `id` int(11) NOT NULL,
+  `title` int(35) NOT NULL,
+  `description` longtext NOT NULL,
+  `type` char(35) NOT NULL,
+  `priority` char(35) NOT NULL,
+  `status` char(11) NOT NULL,
+  `assigned_to` int(11) NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `created` date NOT NULL,
+  `updated` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `firstname` char(35) NOT NULL,
+  `lastname` char(35) NOT NULL,
+  `password` char(35) NOT NULL,
+  `email` char(35) NOT NULL,
+  `date_joined` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `password`, `email`, `date_joined`) VALUES
+(0, 'Admin', 'Gai', 'password123', 'admin@bugme.com', '2019-11-24');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
