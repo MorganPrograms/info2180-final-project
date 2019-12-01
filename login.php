38e3fe424a7c2b632e51ba6e4ed89a65a6e23a6a
RajkeshaSolith <sajonelle@gmail.com>
2019-12-01 18:08:32 -0500

    added link to newuser.html

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ session_start(); //Starting the session
 
 		$email=$_POST['email'];
 		$password=$_POST['password'];
-
+		
 		//mysqli_connect(), this function opens a new connection to the MySql server.
 		$con=mysqli_connect("localhost","root","");
 		mysqli_select_db($con,"schema");
@@ -12,7 +12,7 @@ session_start(); //Starting the session
 		$query= mysqli_query($con, "SELECT email,password FROM users WHERE  email='$email' and password='$password'") or die(mysqli_error($con));
 		$finding= mysqli_fetch_array($query);
 		if ($finding['email']==$email && $finding['password']==$password){
-		echo "I work";
+			header("Location: NewUser.html");
 		}else{
 			echo "Failed";
 		}
