b1f0876d602f14e8352f5b0dd947da17ddb0d4a9
RajkeshaSolith <sajonelle@gmail.com>
2019-12-01 13:03:59 -0500

    add js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	var submitted = document.getElementById("Submit1");
+	/*submitted.addEventListener("click", alert1());
+
+	function alert1(){
+		alert("Issue has been Submitted.Click Home button to view Issue");
+	}*/
+}
